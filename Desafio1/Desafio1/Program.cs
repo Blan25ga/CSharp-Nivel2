abdfccb895e4b307fd28cf771c452ed7e520551d
Blan25ga <gabriel.bl@hotmail.com.ar>
2026-05-11 23:32:51 -0300

    Solucion de desafio 1 (POO)

---
+++ Desafio1/Desafio1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creación de objeto Telefono
+            Telefono telefono1 = new Telefono("Samsung", "Galaxy S21");
+            telefono1.NumeroTelefonico = "123456789"; //Asignación del número telefónico.
+            telefono1.CodigoOperador = 1; //Asignación del código de operador.
+
+            // Mostrar información
+            Console.WriteLine("Marca: " + telefono1.Marca);
+            Console.WriteLine("Modelo: " + telefono1.Modelo);
+            Console.WriteLine("Número: " + telefono1.NumeroTelefonico);
+            Console.WriteLine("Operador: " + telefono1.CodigoOperador);
+
+            // Probar métodos
+            Console.WriteLine(telefono1.Llamar());//Llamada sin contacto específico.
+            Console.WriteLine(telefono1.Llamar("Gabriel"));//Llamada a un contacto específico.
+
+        }
+    }
+}
---
+++ Desafio1/Desafio1/Telefono.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    public class Telefono
+    {
+        /*atributos:
+        Modelo string. //Solo lectura 
+        Marca string. //Solo lectura
+        NumeroTelefonico string. //Lectura y escritura
+        CodigoOperador int (1, 2 o 3) //Lectura y escritura. 
+        Validar escritura que solo admita 1, 2 o 3, caso contrario escribir un cero.//
+        */
+
+
+        private int codigoOperador; //Guardado de datos privados ingresados.
+
+        public string Modelo { get; }
+        public string Marca { get; }
+        public string NumeroTelefonico { get; set; }
+
+        public int CodigoOperador//Propiedad para validar el ingreso de datos.
+        {
+            get { return codigoOperador; }//Retorna el valor guardado.
+            set //Validación para aceptar solo 1, 2 o 3, caso contrario asignar cero.
+            {
+                if (value == 1 || value == 2 || value == 3)
+                {
+                    codigoOperador = value;
+                }
+                else
+                {
+                    codigoOperador = 0;
+                }
+            }
+        }
+
+        // constructor que recibe marca y modelo 
+        public Telefono(string marca, string modelo) 
+        {
+            this.Marca = marca;
+            this.Modelo = modelo;
+        }
+
+        // MÉTODOS
+        public string Llamar() //metodo que retorna un string indicando que se está realizando una llamada.
+        {
+            return "Realizando llamada...";
+        }
+
+        //Sobrecarga del metodo LLamar.
+        public string Llamar(string contacto)
+        {
+            return "Llamando a " + contacto;
+        }
+
+    }
+}
