4dfd4e8a4c84e08d79b245f2e149cf1c42a235e7
Blan25ga <gabriel.bl@hotmail.com.ar>
2026-05-14 21:14:33 -0300

    Se agregan clases para trabajar con Sobre Escrituras de Metodos

---
+++ Poo++/SobreEscrituraMetodos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobreEscrituraMetodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalDomestico animal = new AnimalDomestico();//Creación de un objeto de la clase AnimalDomestico
+        }
+    }
+}
