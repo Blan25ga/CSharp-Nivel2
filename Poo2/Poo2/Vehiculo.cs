dd2f14a8123d2a50ca1ed3acd926a3fb594eb77c
Blan25ga <gabriel.bl@hotmail.com.ar>
2026-05-16 13:38:06 -0300

    Herencia, Colecciones y Metodos

---
+++ Poo2/Poo2/Vehiculo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo2
+{
+    internal class Vehiculo
+    {
+        // Propiedades para almacenar la marca y el modelo del vehículo
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+
+        public Vehiculo(string marca, string modelo) //Constructor para inicializar las propiedades Marca y Modelo
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
+
+        public void Arrancar() // Método (funcion) para simular el arranque del vehículo
+        {
+            Console.WriteLine(Marca + " " + Modelo + " arrancó.");
+        }
+    }
+}
