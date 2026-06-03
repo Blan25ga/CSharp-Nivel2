6fd3b7f71691b51c7fa8f0ad70eacac3963ebcd2
Blan25ga <gabriel.bl@hotmail.com.ar>
2026-06-02 22:45:14 -0300

    Práctica - Validación de Datos Personales. - Implementada validación en campos Apellido, Nombre, Edad y Dirección - Campos vacíos se marcan en rojo - Edad restringida a números (0–99) con KeyPress - Configurados límites de caracteres y conversión automática a mayúsculas - Resultado muestra datos enmascarados con 'X' manteniendo longitud - Botón Cancelar cierra la aplicación

--- DatosPersonales/AppWindows2/Form1.Designer.cs
+++ DatosPersonales/AppWindows2/Form1.Designer.cs
@@ -28,35 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtApellido = new System.Windows.Forms.Label();
+            this.txtAapellido = new System.Windows.Forms.Label();
             this.txtEdad = new System.Windows.Forms.Label();
             this.txtDireccion = new System.Windows.Forms.Label();
             this.txtNombre = new System.Windows.Forms.Label();
             this.txtResultado = new System.Windows.Forms.Label();
             this.btnAceptar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBoxResultado = new System.Windows.Forms.TextBox();
+            this.textBoxApellido = new System.Windows.Forms.TextBox();
+            this.textBoxDireccion = new System.Windows.Forms.TextBox();
+            this.textBoxEdad = new System.Windows.Forms.TextBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // txtApellido
+            // txtAapellido
             // 
-            this.txtApellido.AutoSize = true;
-            this.txtApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.txtApellido.Location = new System.Drawing.Point(36, 31);
-            this.txtApellido.Name = "txtApellido";
-            this.txtApellido.Size = new System.Drawing.Size(75, 17);
-            this.txtApellido.TabIndex = 0;
-            this.txtApellido.Text = "APELLIDO";
-            this.txtApellido.UseMnemonic = false;
+            this.txtAapellido.AutoSize = true;
+            this.txtAapellido.BackColor = System.Drawing.Color.Silver;
+            this.txtAapellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtAapellido.ForeColor = System.Drawing.Color.Black;
+            this.txtAapellido.Location = new System.Drawing.Point(36, 31);
+            this.txtAapellido.Name = "txtAapellido";
+            this.txtAapellido.Size = new System.Drawing.Size(75, 17);
+            this.txtAapellido.TabIndex = 0;
+            this.txtAapellido.Text = "APELLIDO";
+            this.txtAapellido.UseMnemonic = false;
             // 
             // txtEdad
             // 
             this.txtEdad.AutoSize = true;
             this.txtEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtEdad.ForeColor = System.Drawing.Color.Black;
             this.txtEdad.Location = new System.Drawing.Point(36, 108);
             this.txtEdad.Name = "txtEdad";
             this.txtEdad.Size = new System.Drawing.Size(46, 17);
@@ -67,6 +70,7 @@
             // 
             this.txtDireccion.AutoSize = true;
             this.txtDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtDireccion.ForeColor = System.Drawing.Color.Black;
             this.txtDireccion.Location = new System.Drawing.Point(36, 146);
             this.txtDireccion.Name = "txtDireccion";
             this.txtDireccion.Size = new System.Drawing.Size(82, 17);
@@ -77,6 +81,7 @@
             // 
             this.txtNombre.AutoSize = true;
             this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtNombre.ForeColor = System.Drawing.Color.Black;
             this.txtNombre.Location = new System.Drawing.Point(36, 71);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(68, 17);
@@ -87,6 +92,7 @@
             // 
             this.txtResultado.AutoSize = true;
             this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtResultado.ForeColor = System.Drawing.Color.Black;
             this.txtResultado.Location = new System.Drawing.Point(36, 206);
             this.txtResultado.Name = "txtResultado";
             this.txtResultado.Size = new System.Drawing.Size(93, 17);
@@ -96,99 +102,106 @@
             // btnAceptar
             // 
             this.btnAceptar.BackColor = System.Drawing.Color.White;
+            this.btnAceptar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnAceptar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnAceptar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAceptar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.btnAceptar.FlatAppearance.BorderSize = 2;
             this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnAceptar.ForeColor = System.Drawing.Color.Black;
             this.btnAceptar.Location = new System.Drawing.Point(177, 463);
             this.btnAceptar.Name = "btnAceptar";
             this.btnAceptar.Size = new System.Drawing.Size(101, 33);
             this.btnAceptar.TabIndex = 5;
             this.btnAceptar.Text = "Aceptar";
             this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
             // 
             // btnCancelar
             // 
             this.btnCancelar.BackColor = System.Drawing.Color.White;
+            this.btnCancelar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCancelar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.btnCancelar.FlatAppearance.BorderSize = 2;
             this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCancelar.ForeColor = System.Drawing.Color.Black;
             this.btnCancelar.Location = new System.Drawing.Point(318, 463);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(101, 33);
             this.btnCancelar.TabIndex = 6;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
-            // textBox2
+            // textBoxResultado
             // 
-            this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.textBox2.Location = new System.Drawing.Point(39, 234);
-            this.textBox2.MaxLength = 30000;
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(513, 209);
-            this.textBox2.TabIndex = 8;
+            this.textBoxResultado.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBoxResultado.Location = new System.Drawing.Point(39, 234);
+            this.textBoxResultado.MaxLength = 30000;
+            this.textBoxResultado.Multiline = true;
+            this.textBoxResultado.Name = "textBoxResultado";
+            this.textBoxResultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResultado.Size = new System.Drawing.Size(513, 209);
+            this.textBoxResultado.TabIndex = 8;
             // 
-            // textBox1
+            // textBoxApellido
             // 
-            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.textBox1.Location = new System.Drawing.Point(122, 30);
-            this.textBox1.MaxLength = 1500;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(430, 20);
-            this.textBox1.TabIndex = 9;
+            this.textBoxApellido.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBoxApellido.Location = new System.Drawing.Point(122, 30);
+            this.textBoxApellido.MaxLength = 1500;
+            this.textBoxApellido.Name = "textBoxApellido";
+            this.textBoxApellido.Size = new System.Drawing.Size(430, 20);
+            this.textBoxApellido.TabIndex = 9;
             // 
-            // textBox3
+            // textBoxDireccion
             // 
-            this.textBox3.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.textBox3.Location = new System.Drawing.Point(122, 146);
-            this.textBox3.MaxLength = 1500;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(430, 20);
-            this.textBox3.TabIndex = 10;
+            this.textBoxDireccion.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBoxDireccion.Location = new System.Drawing.Point(122, 146);
+            this.textBoxDireccion.MaxLength = 1500;
+            this.textBoxDireccion.Name = "textBoxDireccion";
+            this.textBoxDireccion.Size = new System.Drawing.Size(430, 20);
+            this.textBoxDireccion.TabIndex = 10;
             // 
-            // textBox4
+            // textBoxEdad
             // 
-            this.textBox4.Location = new System.Drawing.Point(122, 104);
-            this.textBox4.MaxLength = 3;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(115, 20);
-            this.textBox4.TabIndex = 11;
+            this.textBoxEdad.Location = new System.Drawing.Point(122, 104);
+            this.textBoxEdad.MaxLength = 3;
+            this.textBoxEdad.Name = "textBoxEdad";
+            this.textBoxEdad.Size = new System.Drawing.Size(51, 20);
+            this.textBoxEdad.TabIndex = 11;
+            this.textBoxEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxEdad_KeyPress);
             // 
-            // textBox5
+            // textBoxNombre
             // 
-            this.textBox5.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.textBox5.Location = new System.Drawing.Point(122, 71);
-            this.textBox5.MaxLength = 1500;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(430, 20);
-            this.textBox5.TabIndex = 12;
+            this.textBoxNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBoxNombre.Location = new System.Drawing.Point(122, 71);
+            this.textBoxNombre.MaxLength = 1500;
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(430, 20);
+            this.textBoxNombre.TabIndex = 12;
             // 
             // DatosPersonales
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackColor = System.Drawing.Color.Silver;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(599, 529);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(596, 529);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.textBoxEdad);
+            this.Controls.Add(this.textBoxDireccion);
+            this.Controls.Add(this.textBoxApellido);
+            this.Controls.Add(this.textBoxResultado);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.txtResultado);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.txtDireccion);
             this.Controls.Add(this.txtEdad);
-            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtAapellido);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "DatosPersonales";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -202,18 +215,18 @@
 
         #endregion
 
-        private System.Windows.Forms.Label txtApellido;
+        private System.Windows.Forms.Label txtAapellido;
         private System.Windows.Forms.Label txtEdad;
         private System.Windows.Forms.Label txtDireccion;
         private System.Windows.Forms.Label txtNombre;
         private System.Windows.Forms.Label txtResultado;
         private System.Windows.Forms.Button btnAceptar;
         private System.Windows.Forms.Button btnCancelar;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBoxResultado;
+        private System.Windows.Forms.TextBox textBoxApellido;
+        private System.Windows.Forms.TextBox textBoxDireccion;
+        private System.Windows.Forms.TextBox textBoxEdad;
+        private System.Windows.Forms.TextBox textBoxNombre;
     }
 }
 
--- DatosPersonales/AppWindows2/Form1.cs
+++ DatosPersonales/AppWindows2/Form1.cs
@@ -17,11 +17,114 @@ namespace AppWindows2
             InitializeComponent();
         }
 
+        // Evento Load del formulario
         private void DatosPersonales_Load(object sender, EventArgs e)
         {
 
         }
 
-       
+        // Evento Click del botón Aceptar, y de campos de textos para validar que no estén vacíos
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+        
+            bool camposValidos = true; //Bandera para verificar si todos los campos son válidos
+
+            // Nombre
+            if (string.IsNullOrWhiteSpace(textBoxNombre.Text))
+            {
+                textBoxNombre.BackColor = Color.Red;
+                camposValidos = false;
+            }
+            else
+            {
+                textBoxNombre.BackColor = Color.White;
+            }
+
+            // Apellido
+            if (string.IsNullOrWhiteSpace(textBoxApellido.Text))
+            {
+                textBoxApellido.BackColor = Color.Red;
+                camposValidos = false;
+            }
+            else
+            {
+                textBoxApellido.BackColor = Color.White;
+            }
+
+            // Edad
+            if (string.IsNullOrWhiteSpace(textBoxEdad.Text))
+            {
+                textBoxEdad.BackColor = Color.Red;
+                camposValidos = false;
+            }
+            else
+            {
+                textBoxEdad.BackColor = Color.White;
+            }
+
+            // Dirección
+            if (string.IsNullOrWhiteSpace(textBoxDireccion.Text))
+            {
+                textBoxDireccion.BackColor = Color.Red;
+                camposValidos = false;
+            }
+            else
+            {
+                textBoxDireccion.BackColor = Color.White;
+            }
+
+            // Si todos los campos están completos
+            if (camposValidos)
+            {
+                // Muestra datos ingresados
+                /*textBoxResultado.Text = "Apellido y Nombre: " + textBoxApellido.Text + " " + textBoxNombre.Text + Environment.NewLine +
+                                        "Edad: " + textBoxEdad.Text + Environment.NewLine +
+                                        "Dirección: " + textBoxDireccion.Text;
+                */
+                // Muestra el resultado mreeemplazando por "x" los datos originales
+                textBoxResultado.Text = "Apellido y Nombre: "
+                           + new string('X', textBoxApellido.Text.Length) + " "
+                           + new string('X', textBoxNombre.Text.Length) + Environment.NewLine +
+                           "Edad: " + new string('X', textBoxEdad.Text.Length) + Environment.NewLine +
+                           "Dirección: " + new string('X', textBoxDireccion.Text.Length);
+            }
+        }
+
+
+        // Evento Click del botón Cancelar
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();// Cierra el formulario
+
+        }
+
+        // Evento KeyPress del TextBox Edad para validar la entrada de solo números
+        private void textBoxEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Solo permite la entrada de dígitos y la tecla de retroceso
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true; // Ignora la tecla presionada
+            }
+
+            //Valida que la edad esté dentro de un rango
+            if (int.TryParse(textBoxEdad.Text, out int edad))
+            {
+                if (edad < 0 || edad > 99)
+                {
+                    textBoxEdad.BackColor = Color.Red;
+                }
+                else
+                {
+                    textBoxEdad.BackColor = Color.White;
+                }
+
+            }
+            else
+            {
+                // Si está vacío o no es número
+                textBoxEdad.BackColor = Color.Red;
+            }
+        }
     }
 }
